5fb44f7d3206b891d4bf45fb56036f917f1a3066
gitforschool <edrick@ualberta.ca>
2014-10-06 02:02:10 -0600

    Added Test, method and classes(Author,Question,Answer,Reply) as well as wiki folder

---
+++ Team09_Q'sAndA's/src/com/team09/qanda/Answer.java
@@ -0,0 +1,24 @@
+package com.team09.qanda;
+
+public class Answer {
+
+	public Answer(String answer, Author a2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void AddReply(Reply r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int replyCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void insertImage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Team09_Q'sAndA's/src/com/team09/qanda/Reply.java
@@ -0,0 +1,9 @@
+package com.team09.qanda;
+
+public class Reply {
+
+	public Reply(String reply, Author a3) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
