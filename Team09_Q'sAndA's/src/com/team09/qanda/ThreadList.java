83ad574f3838d6882ba46a4c86773f220c7209f9
GuyShane <brasss@hotmail.com>
2014-10-08 13:59:52 -0600

    Made ThreadList smaller

--- Team09_Q'sAndA's/src/com/team09/qanda/ThreadList.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/ThreadList.java
@@ -5,29 +5,26 @@ import java.util.ArrayList;
 public class ThreadList {
 	private ElasticSearchHandler esh;
 	private ArrayList<QuestionThread> threads;
-	private ArrayList<Post> questions;
 	
 	public ThreadList() {
 		retrieveThreads();
-		populateQuestions();
 	}
 	
 	private void retrieveThreads() {
 		this.threads=esh.getThreads();
 	}
-	
-	private void populateQuestions() {
-		for (QuestionThread t:threads) {
-			questions.add(t.getQuestion());
-		}
-	}
+
 	
 	public ArrayList<QuestionThread> getThreads() {
 		return this.threads;
 	}
 	
 	public ArrayList<Post> getQuestions() {
-		return this.questions;
+		ArrayList<Post> questions=new ArrayList<Post>();
+		for (QuestionThread t:threads) {
+			questions.add(t.getQuestion());
+		}
+		return questions;
 	}
 	
 	public void addThread(QuestionThread thread) {
