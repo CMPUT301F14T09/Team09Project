48d06b96583f57a6ef400916c33ae52426ef9497
GuyShane <brasss@hotmail.com>
2014-10-08 13:45:28 -0600

    Decoupled models and controllers a bit

--- Team09_Q'sAndA's/src/com/team09/qanda/Reply.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/Reply.java
@@ -4,6 +4,11 @@ public class Reply {
 	private String text;
 	private User author;
 	
+	public Reply(User author, String text) {
+		this.author=author;
+		this.text=text;
+	}
+	
 	public String getText() {
 		return this.text;
 	}
--- Team09_Q'sAndA's/src/com/team09/qanda/User.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/User.java
@@ -3,6 +3,18 @@ package com.team09.qanda;
 public class User {
 	private String name;
 	
+	public User() {
+		this.name="User"+System.currentTimeMillis();
+	}
+	
+	public User(String name) {
+		this.name=name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
 	public String getName() {
 		return this.name;
 	}
