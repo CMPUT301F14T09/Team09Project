1276362c81bfeeefdac808aa70cec436dca5a83e
GuyShane <brasss@hotmail.com>
2014-10-08 16:22:53 -0600

    resolved conflict changed name to ThreadListController

--- Team09_Q'sAndA's/src/com/team09/qanda/Reply.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/Reply.java
@@ -1,7 +1,6 @@
 package com.team09.qanda;
 
 public class Reply {
-<<<<<<< HEAD
 	private String text;
 	private User author;
 	
@@ -16,11 +15,6 @@ public class Reply {
 	
 	public User getAuthor() {
 		return this.author;
-=======
-
-	public Reply(String reply, Author a3) {
-		// TODO Auto-generated constructor stub
->>>>>>> 7757e9a4fc6739c0b4b475d88d512b2906e81b4a
 	}
 
 }
--- Team09_Q'sAndA's/src/com/team09/qanda/ThreadListController.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/ThreadListController.java
@@ -1,11 +1,11 @@
 package com.team09.qanda;
 
 
-public class QuestionController {
+public class ThreadListController {
 	//Handles the questions displayed to the user on the main screen of the application
 	private ThreadList tl;
 	
-	public QuestionController(ThreadList threadList) {
+	public ThreadListController(ThreadList threadList) {
 		this.tl=threadList;
 	}
 	
