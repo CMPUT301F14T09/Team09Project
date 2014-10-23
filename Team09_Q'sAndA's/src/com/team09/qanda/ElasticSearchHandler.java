899c93bfe2ee3c03711f6a423227e4481e6749d1
hjklz <aayao@ualberta.ca>
2014-10-23 10:26:16 -0600

    added ESH get functionality partially

--- Team09_Q'sAndA's/src/com/team09/qanda/ElasticSearchHandler.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/ElasticSearchHandler.java
@@ -4,15 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.fluent.Content;
-import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -33,15 +31,67 @@ public class ElasticSearchHandler {
 		String URL = "http://localhost:9200/test/";
 	}
 	
-	//sortStyle is id of the element found at the
-	//the index of the spinner element found in the string resource
-	public ArrayList<QuestionThread> getThreads(int numQuestions) {
-		int sortStyle = 0;
-		return new ArrayList<QuestionThread>();
+	/**
+	 * get the http response and return json string
+	 * copied from lab Oct 23,2014
+	 */
+	String getEntityContent(HttpResponse response) throws IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader((response.getEntity().getContent())));
+		String output;
+		System.err.println("Output from Server -> ");
+		String json = "";
+		while ((output = br.readLine()) != null) {
+			System.err.println(output);
+			json += output;
+		}
+		System.err.println("JSON:"+json);
+		return json;
 	}
 	
 	//sortStyle is id of the element found at the
 	//the index of the spinner element found in the string resource
+	//this is default version
+	//could change the design of this method to reduce repetition
+	public ArrayList<QuestionThread> getThreads(int numQuestions) {
+		int sortStyle = 0;
+		ThreadList thread = null;
+		try{
+			//TODO:http string currently invalid need to deal with ids
+			HttpGet getRequest = new HttpGet(URL+"999?pretty=1");
+
+			getRequest.addHeader("Accept","application/json");
+
+			HttpResponse response = httpclient.execute(getRequest);
+
+			String status = response.getStatusLine().toString();
+			System.out.println(status);
+
+			String json = getEntityContent(response);
+
+			// We have to tell GSON what type we expect
+			Type elasticSearchResponseType = new TypeToken<ElasticSearchResponse<ThreadList>>(){}.getType();
+			// Now we expect to get a Recipe response
+			ElasticSearchResponse<ThreadList> esResponse = gson.fromJson(json, elasticSearchResponseType);
+			// We get the recipe from it!
+			thread = esResponse.getSource();
+			
+			getRequest.releaseConnection();
+
+		} catch (ClientProtocolException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		return thread.getThreads();
+	}
+	
+	//sortStyle is id of the element found at the
+	//the index of the spinner element found in the string resource
+	//overloaded more specific version, for sorting
 	public ArrayList<QuestionThread> getThreads(int sortStyle,int numQuestions) {
 		return new ArrayList<QuestionThread>();
 	}
@@ -52,6 +102,8 @@ public class ElasticSearchHandler {
 	public boolean saveThreads(ThreadList thread) throws IllegalStateException, IOException {
 		HttpPost httpPost = new HttpPost(URL);
 		StringEntity stringentity = null;
+		
+		//TODO:need to change this section to deal with QuestionThreads and not ThreadList
 		try {
 			stringentity = new StringEntity(gson.toJson(thread));
 		} catch (UnsupportedEncodingException e) {
