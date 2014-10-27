75ed6b1e7043bdea2a663b1c19fcc5e46de0c996
Byron Maroney <bmaroney@ualberta.ca>
2014-10-26 18:56:58 -0600

    Added minimal code for search (included placeholder the for ESH)

--- Team09_Q'sAndA's/src/com/team09/qanda/SearchActivity.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/SearchActivity.java
@@ -4,9 +4,12 @@ package com.team09.qanda;
  * This searching is necessary for searching 
  */
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SearchActivity extends Activity {
 
@@ -14,6 +17,7 @@ public class SearchActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
+		getSearchQuery(getIntent());
 	}
 
 	@Override
@@ -33,5 +37,17 @@ public class SearchActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	private void getSearchQuery(Intent intent){
+		
+		if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			// Add code for searching (for the the ESH) here
+			
+		}
+		else{
+			Toast.makeText(this, "No Search Query... closing the activity",Toast.LENGTH_LONG).show();
+			finish();
+		}
 	}
 }
