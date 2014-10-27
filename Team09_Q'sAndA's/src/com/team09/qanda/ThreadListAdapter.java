9b3078662f3bada84bc9e139e29e2412ae3a8eaf
Byron Maroney <bmaroney@ualberta.ca>
2014-10-26 18:45:55 -0600

    -Deprecated sort routines (no longer needed) and changed the name of the 	adapter
    - Added tests for the Main Activity (reused test from the old testclass,
    SortedArrayAdapter)

--- Team09_Q'sAndA's/src/com/team09/qanda/ThreadListAdapter.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/ThreadListAdapter.java
@@ -3,15 +3,23 @@ package com.team09.qanda;
 import java.util.Comparator;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-public class SortedArrayAdapter extends ArrayAdapter<QuestionThread> {
+public class ThreadListAdapter extends ArrayAdapter<QuestionThread> {
 
-	public SortedArrayAdapter(Context context, int textViewResourceId) {
+	public ThreadListAdapter(Context context, int textViewResourceId) {
 		super(context, textViewResourceId);
 		// TODO Auto-generated constructor stub
 	}
-
+	@Override
+	//This is need so that we can have a custom textview (to represent questions
+	public View getView( int position, View convertView, ViewGroup parent){
+		//ToDO
+		return convertView;
+	}
+	@Deprecated
 	public void sortByHasPictures() {
 		sort(new Comparator<QuestionThread>(){
 			@Override
@@ -27,7 +35,7 @@ public class SortedArrayAdapter extends ArrayAdapter<QuestionThread> {
 			
 		});
 	}
-
+	@Deprecated
 	public void sortByMostRecent() {
 		sort(new Comparator<QuestionThread>(){
 			@Override
@@ -43,7 +51,7 @@ public class SortedArrayAdapter extends ArrayAdapter<QuestionThread> {
 			
 		});
 	}
-
+	@Deprecated
 	public void sortByLeastUpVoted() {
 		sort(new Comparator<QuestionThread>(){
 			@Override
@@ -54,7 +62,7 @@ public class SortedArrayAdapter extends ArrayAdapter<QuestionThread> {
 		});
 		
 	}
-
+	@Deprecated
 	public void sortByMostUpVoted() {
 		sort(new Comparator<QuestionThread>(){
 			@Override
@@ -70,7 +78,7 @@ public class SortedArrayAdapter extends ArrayAdapter<QuestionThread> {
 			
 		});
 	}
-
+	@Deprecated
 	public void sortByOldest() {
 		sort(new Comparator<QuestionThread>(){
 			@Override
