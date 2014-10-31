104bbc65966f487ab00735b0355200146ffc798a
msabbasi <msabbasi@ualberta.ca>
2014-10-30 23:28:51 -0600

    Partial display thread functionality

--- Team09_Q'sAndA's/src/com/team09/qanda/QuestionThreadActivity.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/QuestionThreadActivity.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class QuestionThreadActivity extends Activity {
 	
 	private QuestionThread thread;
+	private ThreadAdapter adapter;
+	private ListView threadPostsList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,8 @@ public class QuestionThreadActivity extends Activity {
 		
 		thread = (QuestionThread) getIntent().getExtras().getSerializable("Selected Thread");
 		
+		threadPostsList = (ListView) findViewById(R.id.ThreadPostsView);
+		/*
 		TextView question = (TextView) findViewById(R.id.threadQuestion);
 		question.setText(thread.getQuestion().getText());
 		
@@ -26,6 +34,8 @@ public class QuestionThreadActivity extends Activity {
 		TextView upvotes = (TextView) findViewById(R.id.questionUpvotes);
 		upvotes.setText(thread.getQuestion().getUps() + " Point(s)");
 		
+		TextView 
+		*/
 	}
 
 	@Override
@@ -45,5 +55,29 @@ public class QuestionThreadActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		
+		//ArrayList<QuestionThread> testthreads = new ArrayList<QuestionThread>();
+		//testthreads.add(new QuestionThread(new Post(new User(), "Question 2?")));
+		//testAdapter = new ArrayAdapter<QuestionThread>(this,R.layout.list_item, testthreads);
+		adapter = new ThreadAdapter(this, R.layout.thread_row_layout, thread);
+		threadPostsList.setAdapter(adapter);
+		
+		/*
+		mainThreadsList.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				QuestionThread selectedThread = (QuestionThread) parent.getItemAtPosition(position);
+				displayThread(selectedThread);
+												
+			}
+		});
+		*/
 	}
 }
