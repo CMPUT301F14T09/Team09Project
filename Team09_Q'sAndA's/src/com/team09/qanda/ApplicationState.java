cbc52a338a514be13440ff3c7a4f516a8aa6bbc8
hjklz <aayao@ualberta.ca>
2014-11-07 13:46:16 -0700

    fixed weird spacing and static discrepancy

--- Team09_Q'sAndA's/src/com/team09/qanda/ApplicationState.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/ApplicationState.java
@@ -8,24 +8,24 @@ package com.team09.qanda;
 
 public class ApplicationState {
 	private static ApplicationState appState = null;
-	private static User user;
+	private User user;
 	
 	protected ApplicationState(){
 		
 	}
 	
-   public static ApplicationState getInstance() {
-	      if(appState == null) {
-	    	  appState = new ApplicationState();
-	      }
-	      return appState;
-   }
+	public static ApplicationState getInstance() {
+		if(appState == null) {
+			appState = new ApplicationState();
+		}
+		return appState;
+	}
    
-   public User getUser(){
-	   return user;
-   }
+	public User getUser(){
+		return user;
+	}
    
-   public void setUser(User user){
-	   this.user = user;
-   }
+	public void setUser(User user){
+		this.user = user;
+	}
 }
