2c78dddddae90c35a549b352beb6f3e285b8c0ba
Byron Maroney <bmaroney@ualberta.ca>
2014-11-29 21:25:48 -0700

    Added documentation for the Replies class

--- Team09_Q'sAndA's/src/com/team09/qanda/models/Reply.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/models/Reply.java
@@ -17,27 +17,48 @@ public class Reply extends QModel<QView> implements Serializable {
 	private User author;
 	private Date timestamp;
 	
+	/**
+	 * A Reply is used to represent a response to a Post 
+	 * @param author, the User responsible for the reply
+	 * @param text, the String representation of the reply
+	 */
 	public Reply(User author, String text) {
 		this.author=author;
 		this.text=text;
 		this.timestamp=new Date();
 	}
 	
-	//overloaded constructor to test default order
+	/**
+	 * This constructor allows for a REply to be instantiated with a
+	 * custom date. It is intended for testing purposes only
+	 * @param author, the User responsible for the reply
+	 * @param text, the String representation of the reply
+	 * @param date, The date that this object was/will be created
+	 */
 	public Reply(User author, String text, Date date) {
 		this.author=author;
 		this.text=text;
 		this.timestamp= date;
 	}
-	
+	/**
+	 * Get the String representation of the Reply
+	 * @return the String representation of the Reply
+	 */
 	public String getText() {
 		return this.text;
 	}
-	
+	/**
+	 * Get the User that is responsible for this Reply.
+	 * @return the Author, the User object which is associated with this Reply
+	 */
 	public User getAuthor() {
 		return this.author;
 	}
-
+	/**
+	 * The timestamp of this Post refers to
+	 * to the Date and time (represented by the Java Date class) when the Reply object was first instantiated
+	 * @return the Date object associated with the particular date and time when the Reply was first created. 
+	 */
 	public Date getTimestamp() {
 		return this.timestamp;
 	}
