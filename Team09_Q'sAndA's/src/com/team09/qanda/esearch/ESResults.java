d7077f9863370aff3cba89cd68759ebe83afbfb3
GuyShane <brasss@hotmail.com>
2014-11-30 12:47:08 -0700

    ElasticSeacrh support class documentation.

--- Team09_Q'sAndA's/src/com/team09/qanda/esearch/ESResults.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/esearch/ESResults.java
@@ -1,6 +1,12 @@
 package com.team09.qanda.esearch;
 
-
+/**
+ * 
+ * This class is used to obtain information from the elasticsearch server.
+ * It is not used in the context of the application other than to store
+ * the result of queries to the server.
+ *
+ */
 public class ESResults {
 	private int took;
 	private boolean timed_out;
--- Team09_Q'sAndA's/src/com/team09/qanda/esearch/Shards.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/esearch/Shards.java
@@ -1,5 +1,10 @@
 package com.team09.qanda.esearch;
 
+/**
+ * This class contains information about the shards from
+ * a query to the elasticsearch server.
+ *
+ */
 public class Shards {
 	private int total;
 	private int successful;
