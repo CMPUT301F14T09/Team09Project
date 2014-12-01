10de9f67e333759aafead1491452a77176c301e5
msabbasi <msabbasi@ualberta.ca>
2014-11-30 18:57:29 -0700

    Mooore documentation

--- Team09_Q'sAndA's/src/com/team09/qanda/ThreadListAdapter.java
+++ Team09_Q'sAndA's/src/com/team09/qanda/ThreadListAdapter.java
@@ -21,6 +21,10 @@ import com.team09.qanda.models.QuestionThread;
  * and in the UserThreadsActivity to display locally stored questions
  * with a few changes.
  *
+ * This class extends ArrayAdapter<QuestionThread> and uses an ArrayList
+ * of QuestionThread objects to display the summary of a list of questions using
+ * a custom layout.
+ *
  */
 
 public class ThreadListAdapter extends ArrayAdapter<QuestionThread> {
@@ -32,6 +36,17 @@ public class ThreadListAdapter extends ArrayAdapter<QuestionThread> {
 	private int resId;
 	private boolean isMain;
 	
+	
+	/**
+	 * The constructor takes the context of the activity using the adapter, the
+	 * resource id of the custom row layout and the QuestionThread objects to be displayed
+	 * in the form of an array list.
+	 * 
+	 * @param context The context of the activity where the adapter is being used
+	 * @param layoutResourceId The resource id for the custom row layout that is to be used
+	 * @param threads The list of question threads whose summary is to be displayed
+	 * @param isMain A boolean to identify if the adapter is being used in the main activity or not
+	 */
 	public ThreadListAdapter(Context context, int layoutResourceId, ArrayList<QuestionThread> threads, boolean isMain) {
 		super(context, layoutResourceId, threads);
 		this.threads = threads;
@@ -42,6 +57,21 @@ public class ThreadListAdapter extends ArrayAdapter<QuestionThread> {
 		this.ids=lsh.getIds(context, Constants.LATER_IDS_FILENAME);
 	}
 	
+	/**
+	 * Returns a View that displays a row in the list of questions' summary
+	 * 
+	 * It inflates a custom layout that consists of various text views to display the summary
+	 * of a question. It grabs all the information (text, author, location,
+	 * number of points, and the number of answers) from the QuestionThread object
+	 * at the index in the threads ArrayList that the current position in the list corresponds to.
+	 * 
+	 * This method also implements onClickListeners for the button that is used to save questions
+	 * for later.
+	 * 
+	 * @param position The index of the row in the list being displayed
+	 * @param convertView View used in the previous row
+	 * @param parent The parent that this view will be attached to
+	 */
 	@Override
 	public View getView(final int position, View convertView, ViewGroup parent) {
 
